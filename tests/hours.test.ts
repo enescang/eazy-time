5ed7274a7c6694abe37d17163e9736036aa445a2
enescang <developerindiee@gmail.com>
2023-05-06 22:47:58 +0300

    Hour test cases

--- tests/hours.test.ts
+++ tests/hours.test.ts
@@ -13,7 +13,18 @@ const EazyTimeTest: EazyTimeStructure = CustomEazyTime(config);
 
 describe("Eazy Time Hour Test", () => {
     it("Test The Static Hour Fields", () => {
-        expect(EazyTimeTest.OneHour).toBe(config.BASE_HOUR);
+        expect(EazyTimeTest.OneHour).toBe(config.BASE_HOUR*1);
         expect(EazyTimeTest.TwoHour).toBe(config.BASE_HOUR*2);
+        expect(EazyTimeTest.ThreeHour).toBe(config.BASE_HOUR*3);
+        expect(EazyTimeTest.FourHour).toBe(config.BASE_HOUR*4);
+        expect(EazyTimeTest.FiveHour).toBe(config.BASE_HOUR*5);
+        expect(EazyTimeTest.SixHour).toBe(config.BASE_HOUR*6);
+        expect(EazyTimeTest.SevenHour).toBe(config.BASE_HOUR*7);
+        expect(EazyTimeTest.EightHour).toBe(config.BASE_HOUR*8);
+        expect(EazyTimeTest.NineHour).toBe(config.BASE_HOUR*9);
+        expect(EazyTimeTest.TenHour).toBe(config.BASE_HOUR*10);
+
+        const threeAndAHalfHours = config.BASE_HOUR * 3.5;
+        expect(EazyTimeTest.Hour(3.5)).toBe(threeAndAHalfHours);
     });
 });
